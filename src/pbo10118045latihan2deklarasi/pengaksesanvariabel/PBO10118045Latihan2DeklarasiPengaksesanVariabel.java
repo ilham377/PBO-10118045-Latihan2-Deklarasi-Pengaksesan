16beec9214fd117bbc414824a52a350288ea08e0
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 20:39:13 +0700

    first commit

---
+++ src/pbo10118045latihan2deklarasi/pengaksesanvariabel/PBO10118045Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan2deklarasi.pengaksesanvariabel;
+
+/**
+ *
+ * @author
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : Deklarasi & Pengaksesan Variabel
+ */
+public class PBO10118045Latihan2DeklarasiPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //Konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter = 'D';
+        
+        //menampilkan ke variabel
+        nilaiInt = 50;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // menampilkan hasil
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = " +nilaiInt);
+        System.out.println("Isi Variabel PHI = " +PHI);
+        System.out.println("Isi Variabel Logika = " +nilaiLogika);
+        System.out.println("Isi Variabel Karakter = " +nilaiKarakter);
+        
+        
+        
+        
+        
+    }
+    
+}
